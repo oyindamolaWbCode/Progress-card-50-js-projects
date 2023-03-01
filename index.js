0cd93cfd2d8bb3fe41df39f55da098a7159b5d18
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-03-01 04:34:23 +0100

    DOM assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+const cirles = document.querySelectorAll('.circle');
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
